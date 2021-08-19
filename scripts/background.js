d1f75573a07bbef18b82e586fe37f342a7b49c6b
microwavedram <89029844+microwavedram@users.noreply.github.com>
2021-08-19 13:36:08 +0100

    Cool Background thing

---
+++ scripts/background.js
@@ -0,0 +1,130 @@
+const canvas = document.getElementById("canvas1")
+const ctx = canvas.getContext('2d')
+canvas.width = window.innerWidth
+canvas.height = window.innerHeight
+
+let particlesArray;
+
+let mouse = {
+    x: null,
+    y: null,
+    radius: (canvas.height/100)*(canvas.width/100)
+}
+
+ window.addEventListener('mousemove',
+    function(event) {
+        mouse.x = event.x
+        mouse.y = event.y
+    }
+)
+
+class Particle {
+    constructor(x, y, directionX, directionY, size, color) {
+        this.x = x;
+        this.y = y;
+        this.directionX = directionX;
+        this.directionY = directionY;
+        this.size = size;
+        this.color = color
+    }
+
+    draw() {
+        ctx.beginPath();
+        ctx.arc(this.x,this.y, this.size, 0, Math.PI * 2,false);
+        ctx.fillStyle = '#ffffff';
+        ctx.fill();
+    }
+
+    update() {
+        if (this.x > canvas.width || this.x < 0) {
+            this.directionX = -this.directionX;
+        }
+        if (this.y > canvas.height || this.y < 0) {
+            this.directionY = -this.directionY;
+        }
+
+        let dx = mouse.x - this.x
+        let dy = mouse.y - this.y
+        let distance = Math.sqrt(dx*dx + dy*dy)
+        if (distance < mouse.radius + this.size) {
+            if (mouse.x < this.x && this.x < canvas.width - this.size * 10) {
+                this.x += 10;
+            }
+            if (mouse.x > this.x && this.x > this.size * 10) {
+                this.x -= 10;
+            }
+            if (mouse.y < this.y && this.y < canvas.height - this.size * 10) {
+                this.y += 10;
+            }
+            if (mouse.y > this.y && this.y > this.size * 10) {
+                this.y -= 10;
+            }
+        }
+        this.x += this.directionX;
+        this.y += this.directionX
+
+        this.draw();
+    }
+}
+
+function init() {
+    particlesArray = []
+    let numberOfParticle = (canvas.height * canvas.width) / 9000
+    for (let i = 0; i < numberOfParticle; i++) {
+        let size = (Math.random() * 5) + 1;
+        let x = (Math.random() * ((innerWidth - size * 2) - (size * 2)) + size * 2);
+        let y = (Math.random() * ((innerHeight - size * 2) - (size * 2)) + size * 2);
+        let directionX = (Math.random() * 5) - 2.5;
+        let directionY = (Math.random() * 5) - 2.5;
+        let color = '#ffffff'
+        
+        particlesArray.push(new Particle(x,y,directionX,directionY,size,color))
+    }
+}
+
+function connect() {
+    let opacityValue = 1;
+    for (let a = 0; a< particlesArray.length; a++) {
+        for (let b = a; b < particlesArray.length; b++) {
+            let distance = ((particlesArray[a].x - particlesArray[b].x)*(particlesArray[a].x - particlesArray[b].x) + (particlesArray[a].y - particlesArray[b].y)*(particlesArray[a].y - particlesArray[b].y));
+            if (distance < (canvas.width/7) * (canvas.height/7)) {
+                opacityValue = 1 - (distance/20000)
+                ctx.strokeStyle = 'rgba(255,255,255,'+opacityValue+')';
+                ctx.lineWidth = 1;
+                ctx.beginPath();
+                ctx.moveTo(particlesArray[a].x, particlesArray[a].y)
+                ctx.lineTo(particlesArray[b].x, particlesArray[b].y)
+                ctx.stroke();
+            }
+        }
+    }
+}
+
+function animate() {
+    requestAnimationFrame(animate);
+    ctx.clearRect(0,0,innerWidth, innerHeight);
+
+    for (let i = 0; i < particlesArray.length; i++) {
+        particlesArray[i].update();
+    }
+    connect()
+}
+
+window.addEventListener('resize',
+    function() {
+        canvas.width = innerWidth;
+        canvas.height = innerHeight;
+        mouse.radius = ((canvas.height/80) * (canvas.height/80));
+        init();
+    }
+)
+
+window.addEventListener('mouseout',
+    function() {
+        mouse.x = undefined;
+        mouse.y = undefined;
+    }
+)
+
+init()
+animate()
