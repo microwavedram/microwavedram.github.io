5bffdb2e9e645d5ef123f5508c814b1345566056
microwavedram <89029844+microwavedram@users.noreply.github.com>
2024-12-02 23:35:30 +0000

    new data

---
+++ cors/index.cjs
@@ -0,0 +1,14 @@
+var host = process.env.HOST || "0.0.0.0"
+// Listen on a specific port via the PORT environment variable
+var port = process.env.PORT || 1235
+
+var cors_proxy = require("cors-anywhere")
+cors_proxy
+	.createServer({
+		originWhitelist: [], // Allow all origins
+		requireHeader: ["x-kill-me-please"],
+		removeHeaders: ["cookie", "cookie2"],
+	})
+	.listen(port, host, function () {
+		console.log("Running CORS Anywhere on " + host + ":" + port)
+	})
